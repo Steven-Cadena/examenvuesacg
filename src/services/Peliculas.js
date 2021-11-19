8397e9cb84228e0c4befe4e075e650ad18e53510
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-11-19 14:17:00 +0100

    commit

--- src/services/Peliculas.js
+++ src/services/Peliculas.js
@@ -23,23 +23,23 @@ export default class ServiciosPeliculas{
         });
     }
 
-    // getPeliculasNacionalidad(id){
-    //     return new Promise(function(resolve){
-    //         var request = "api/nacionalidades/" + id;
-    //         var url = Global.urlapipeliculas + request;
-    //         axios.get(url).then(res => {
-    //             resolve(res.data);
-    //         })
-    //     });
-    // }
+    getPeliculasNacionalidad(id){
+        return new Promise(function(resolve){
+            var request = "api/nacionalidades/" + id;
+            var url = Global.urlapipeliculas + request;
+            axios.get(url).then(res => {
+                resolve(res.data);
+            })
+        });
+    }
 
-    // getPeliculasGenero(id){
-    //     return new Promise(function(resolve){
-    //         var request = "api/generos/" + id;
-    //         var url = Global.urlapipeliculas + request;
-    //         axios.get(url).then(res => {
-    //             resolve(res.data);
-    //         })
-    //     });
-    // }
+    getPeliculasGenero(id){
+        return new Promise(function(resolve){
+            var request = "api/generos/" + id;
+            var url = Global.urlapipeliculas + request;
+            axios.get(url).then(res => {
+                resolve(res.data);
+            })
+        });
+    }
 }
